94d9e583cd17c9534297ab2763010c3c981f9a99
Phillip R. Jaenke <prj@rootwyrm.com>
2021-02-02 08:57:28 -0800

    fix the install deps script.

--- container/tc_docker/application/build/00.install_deps.sh
+++ container/tc_docker/application/build/00.install_deps.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-## application/build/00.install_deps.sh
 ################################################################################
 # Copyright (C) 2015-* Phillip R. Jaenke <talecaster@rootwrym.com>
 #
 # Licensed under CC-BY-NC-4.0
 # See /LICENSE for details
-######################################################################
-## Install APKs.
-######################################################################
+################################################################################
+## application/build/00.install_deps.sh
+
+. /opt/talecaster/lib/deploy.lib.sh
 
 SERVICE=$(cat /opt/talecaster/id.service)
 
@@ -16,7 +16,7 @@ function install_deps()
 	if [ -f /opt/talecaster/${SERVICE}.deps ]; then
 		apk --no-cache add $(cat /opt/talecaster/${SERVICE}.deps)
 	elif [ -z $SERVICE ] || [ ! -f /opt/talecaster/id.service ]; then
-		echo "No service assigned."
+		log "No service assigned."
 	else
 		log "No dependencies for ${SERVICE}"
 	fi
