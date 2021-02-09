31ff68bef51dc8eabd0e9b509cd5c396f434ee9e
Phillip R. Jaenke <prj@rootwyrm.com>
2021-02-08 18:19:09 -0800

    Initial commit of transmission.

---
+++ container/tc_transmission/application/build/20.transmission.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+###############################################################################
+# TaleCaster - https://github.com/rootwyrm/talecaster
+# Copyright (C) 2015-* Phillip R. Jaenke <talecaster@rootwrym.com> and its
+# contributors, all rights reserved.
+#
+# Licensed under CC-BY-NC-4.0
+# See /LICENSE for details
+################################################################################
+## application/build/20.transmission.sh
+
+. /opt/talecaster/lib/talecaster.lib.sh
+
+function noop()
+{
+	## Build our configuration...
+	LOCALIP=$(hostname -i)
+	CONFDIR=/talecaster/config
+	SETTINGS=/talecaster/config/settings.json
+	transmission_password=${transmission_password:"t4l3caster"}
+}
+
+noop
