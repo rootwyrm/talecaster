f50e89576a4fd2c335eead7df6621c2a8d3b4a01
Phillip R. Jaenke <prj@rootwyrm.com>
2021-02-20 17:59:46 -0500

    Fix dependency installer, had some dangling log() usage.

--- container/tc_docker/application/build/00.install_deps.sh
+++ container/tc_docker/application/build/00.install_deps.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ################################################################################
-# Copyright (C) 2015-* Phillip R. Jaenke <talecaster@rootwrym.com>
-#
+# TaleCaster
+# Copyright (C) 2015-* Phillip R. Jaenke <talecaster@rootwyrm.com> and its
+# contributors.
+# All rights reserved
+# 
 # Licensed under CC-BY-NC-4.0
 # See /LICENSE for details
 ################################################################################
@@ -15,10 +18,12 @@ function install_deps()
 {
 	if [ -f /opt/talecaster/${SERVICE}.deps ]; then
 		apk --no-cache add $(cat /opt/talecaster/${SERVICE}.deps)
+		CHECK_ERROR $? install_deps
+
 	elif [ -z $SERVICE ] || [ ! -f /opt/talecaster/id.service ]; then
-		log "No service assigned."
+		LOG "No service assigned."
 	else
-		log "No dependencies for ${SERVICE}"
+		LOG "No dependencies for ${SERVICE}"
 	fi
 }
 
