9ac21cff9f7fb5d57b9fde50353b1c18551fbb33
Phillip R. Jaenke <prj@rootwyrm.com>
2021-11-27 12:37:56 -0500

    More frontend fixes and prettification

--- container/tc_frontend/application/build/20.retrieve_logos.sh
+++ container/tc_frontend/application/build/20.retrieve_logos.sh
@@ -15,23 +15,23 @@
 export IMAGEDIR="/opt/talecaster/html/images"
 
 ## Sonarr
-curl -L https://github.com/Sonarr/Sonarr/blob/develop/Logo/64.png > $IMAGEDIR/sonarr.png
+curl -L https://github.com/Sonarr/Sonarr/raw/develop/Logo/64.png > $IMAGEDIR/sonarr.png
 ## Radarr
-curl -L https://github.com/Radarr/Radarr/blob/develop/Logo/64.png > $IMAGEDIR/radarr.png
+curl -L https://github.com/Radarr/Radarr/raw/develop/Logo/64.png > $IMAGEDIR/radarr.png
 ## Lidarr
-curl -L https://github.com/Lidarr/Lidarr/blob/develop/Logo/64.png > $IMAGEDIR/lidarr.png
+curl -L https://github.com/Lidarr/Lidarr/raw/develop/Logo/64.png > $IMAGEDIR/lidarr.png
 ## Readarr
-curl -L https://github.com/Readarr/Readarr/blob/develop/Logo/64.png > $IMAGEDIR/readarr.png
+curl -L https://github.com/Readarr/Readarr/raw/develop/Logo/64.png > $IMAGEDIR/readarr.png
 ## Mylar
-curl -L https://github.com/mylar3/mylar3/blob/v0.6.1/data/images/mylar_logo_128x128.png > $IMAGEDIR/mylar.png
+curl -L https://github.com/mylar3/mylar3/raw/v0.6.1/data/images/mylar_logo_128x128.png > $IMAGEDIR/mylar.png
 
 ## Nzbget, use their favicon
-curl -L https://github.com/nzbget/nzbget/blob/develop/webui/img/favicon-256x256.png > $IMAGEDIR/nzbget.png
+curl -L https://github.com/nzbget/nzbget/raw/develop/webui/img/favicon-256x256.png > $IMAGEDIR/nzbget.png
 ## qBittorrent, use their internal www images
 ## XXX: only a 32x32, need to find a 64x64!
-curl -L https://github.com/qbittorrent/qBittorrent/blob/master/src/webui/www/public/images/qbittorrent32.png > $IMAGEDIR/qbittorrent.png
+curl -L https://github.com/qbittorrent/qBittorrent/raw/master/src/webui/www/public/images/qbittorrent32.png > $IMAGEDIR/qbittorrent.png
 ## Prowlarr
-curl -L https://github.com/Prowlarr/Prowlarr/blob/develop/Logo/64.png > $IMAGEDIR/prowlarr.png
+curl -L https://github.com/Prowlarr/Prowlarr/raw/develop/Logo/64.png > $IMAGEDIR/prowlarr.png
 
 ## Fix permissions
 chmod 0755 $IMAGEDIR/*png
