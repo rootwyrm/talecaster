2b1cd3853bbc77f3612756c1ea62b78a1ee1a91d
Phillip Jaenke <prj@rootwyrm.com>
2023-04-23 16:23:38 -0400

    Start work on phase1, update sonarr to v4

--- container/tc_sonarr/application/build/20.sonarr.sh
+++ container/tc_sonarr/application/build/20.sonarr.sh
@@ -7,16 +7,30 @@
 # Licensed under CC-BY-NC-4.0
 # See /LICENSE for details
 ################################################################################
-## build/20.nzbget.sh
+## build/20.sonarr.sh
 . /opt/talecaster/lib/talecaster.lib.sh
 
 export app_name="Sonarr"
 export app_url="http://www.sonarr.tv/"
 export app_destdir="/opt/Sonarr"
 
-export BRANCH="phantom-develop"
-export VERSION="3"
-export APPURL="https://services.sonarr.tv/v1/download/main/latest?version=3&os=linux"
+#https://services.sonarr.tv/v1/download/develop/latest?version=4&os=linux-musl&arch=arm64
+export OSARCH="linux-musl"
+export ARCH=$(uname -m)
+case $ARCH in
+	x86*)
+		export ARCH="x64"
+		;;
+	aarch64*)
+		export ARCH="arm64"
+		;;
+	*)
+		echo "Unsupported architecture!"
+		exit 255
+		;;
+esac
+export VERSION="4"
+export APPURL='https://services.sonarr.tv/v1/download/develop/latest?version='${VERSION}'&os='${OSARCH}'&arch='${ARCH}''
 
 ######################################################################
 ## Application Install
